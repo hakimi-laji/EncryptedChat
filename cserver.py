0b4009223318d7f6a2a027284abe136a72f5882f
Hakimi Laji <72840941+hakimi-laji@users.noreply.github.com>
2021-01-07 17:43:50 +0800

    Update cserver.py

--- cserver.py
+++ cserver.py
@@ -6,37 +6,20 @@ from _thread import *
 from Crypto.Cipher import AES
 import random, string, base64
 
-"""The first argument AF_INET is the address domain of the 
-socket. This is used when we have an Internet Domain with 
-any two hosts The second argument is the type of socket. 
-SOCK_STREAM means that data or characters are read in 
-a continuous flow."""
+# socket stuffs
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 
-# checks whether sufficient arguments have been provided  
-#if len(sys.argv) != 3:  
-#   print ("Correct usage: $ python <IP address> <port number>") 
-#  exit()  
-  
-# takes the first argument from command prompt as IP address  
-#IP_address = str(sys.argv[1])  
-  
-# takes second argument from command prompt as port number  
-#Port = int(sys.argv[2])  
-
 # bind socket with inputted ip and port
-IP_address = '192.168.1.10'
-Port = 8081
+#IP_address = '192.168.1.10'
+#Port = 8081
+IP_address = input('IP Address: ')
+Port = input('Port: ')
 server.bind((IP_address, Port)) 
 
-""" 
-listens for 100 active connections. This number can be 
-increased as per convenience. 
-"""
+# listens for 100 active connections. This number can be increased as per convenience. 
 server.listen(100) 
-print ("Waiting...")
-
+print ("Waiting for connections...")
 
 list_of_clients = [] 
 
@@ -54,20 +37,17 @@ def clientthread(conn, addr):
 					print ("<" + addr[0] + "> " + message) 
 
 					# Calls broadcast function to send message to all 
-					message_to_send = "<" + addr[0] + "> " + message 
+					#message_to_send = "<" + addr[0] + "> " + message 
 					broadcast(message, conn) 
 
 				else: 
-					"""message may have no content if the connection 
-					is broken, in this case we remove the connection"""
+					# message may have no content if the connection is broken, in this case we remove the connection
 					remove(conn) 
 
 			except: 
 				continue
 
-"""Using the below function, we broadcast the message to all 
-clients who's object is not the same as the one sending 
-the message """
+# broadcast message to all active clients
 def broadcast(message, connection): 
 	for clients in list_of_clients: 
 		if clients!=connection: 
@@ -79,23 +59,17 @@ def broadcast(message, connection):
 				# if the link is broken, we remove the client 
 				remove(clients) 
 
-"""The following function simply removes the object 
-from the list that was created at the beginning of 
-the program"""
+# removes the object from the list that was created at the beginning of the program
 def remove(connection): 
 	if connection in list_of_clients: 
 		list_of_clients.remove(connection) 
 
 while True: 
 
-	"""Accepts a connection request and stores two parameters, 
-	conn which is a socket object for that user, and addr 
-	which contains the IP address of the client that just 
-	connected"""
+	# Accepts a connection request 
 	conn, addr = server.accept() 
 
-	"""Maintains a list of clients for ease of broadcasting 
-	a message to all available people in the chatroom"""
+	# Maintains a list of clients for ease of broadcasting 
 	list_of_clients.append(conn) 
 
 	# prints the address of the user that just connected 
